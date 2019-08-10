25f80a5c79bd4a8b094e6038961027f7d8a399f7
UdaySarker <sarker450@gmail.com>
2019-08-11 00:18:50 +0600

    author nm

--- template/addjoke.html.php
+++ template/addjoke.html.php
@@ -3,9 +3,5 @@
 		<label>Type your joke here</label>
 		<textarea rows="4" cols="12" name="jokes" class="form-control form-control-sm"></textarea>
 	</div>
-	<div class="form-group">
-		<label for="authorname">Enter Author Name</label>
-		<input type="text" name="author" value="" class="form-control">
-	</div>
 	<input type="submit" name="addjoke" class="btn btn-primary">
 </form>
